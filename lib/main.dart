27b47739b87bd7e99e7def02b990d3c5d784c094
kevin imaho <kevin.imaho@gmail.com>
2022-01-17 09:33:02 +0400

    restructuration de la structure statique par rapport au consigne

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.grey.shade900,
         body: Quiz(),
       ),
     );
   }
 }
 
-class Quiz extends StatelessWidget {
-  const Quiz({Key? key}) : super(key: key);
+class Quiz extends StatefulWidget {
+  @override
+  _QuizState createState() => _QuizState();
+}
 
+class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu fringilla lorem. Donec et vestibulum orci. Cras nunc nulla, gravida vehicula elit a, mollis vehicula dolor. Mauris eu laoreet turpis.",
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white,
+            Expanded(
+              flex: 5,
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    'test application quiz',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
                 ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(top: 150.0),
-              child: Card(
-                color: Colors.green,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
                 child: TextButton(
                   style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    primary: Colors.black,
-                    textStyle: const TextStyle(fontSize: 20),
+                    backgroundColor: Colors.green,
+                    primary: Colors.white,
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                    ),
                   ),
-                  onPressed: () {
-                    print('test');
-                  },
+                  onPressed: () {},
                   child: const Text('True'),
                 ),
               ),
-              height: 90.0,
-              width: 750.0,
             ),
-            Container(
-              margin: EdgeInsets.only(top: 30.0),
-              child: Card(
-                color: Colors.red,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
                 child: TextButton(
                   style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    primary: Colors.black,
+                    backgroundColor: Colors.red,
+                    primary: Colors.white,
                     textStyle: const TextStyle(fontSize: 20),
                   ),
                   onPressed: () {
@@ -73,8 +75,6 @@ class Quiz extends StatelessWidget {
                   child: const Text('False'),
                 ),
               ),
-              height: 90.0,
-              width: 750.0,
             ),
           ],
         ),
